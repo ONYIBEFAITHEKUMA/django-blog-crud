9bed47154a0b69989ae2c7833c3731f9b7431dcf
ONYIBEFAITHEKUMA <ONYIBEFAITHEKUMA@gmail.com>
2025-07-03 16:36:12 +0100

    add a content in the database

--- blog/views.py
+++ blog/views.py
@@ -6,4 +6,9 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.all().order_by('-created_at')
-    return render(request, 'blog/post.html', {'posts': posts})
+    return render(request, 'blog/homepage.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
