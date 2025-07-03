4beac5efd5e6d75674e3ae827695d59a53880970
ONYIBEFAITHEKUMA <ONYIBEFAITHEKUMA@gmail.com>
2025-07-03 17:56:30 +0100

    Worked on comment form, to allow visitor leave a comment in the blog

--- blog/models.py
+++ blog/models.py
@@ -25,10 +25,15 @@ class Post(models.Model):
         return self.title
 
 class Comment(models.Model):
+    name = models.CharField(max_length=80, null=True)
+    email = models.EmailField(max_length=80, blank=True, null=True)
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+   
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=True)
 
     def __str__(self):
         return f'Comment by {self.author.username} on {self.post.title}'
+
+   
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,34 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 
 def post_list(request):
     posts = Post.objects.all().order_by('-created_at')
     return render(request, 'blog/homepage.html', {'posts': posts})
 
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    comments = post.comments.filter(approved=True)
 
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        comment_form = CommentForm()
+
+    return render(request, 'blog/post_detail.html', {
+        'post': post,
+        'comments': comments,
+        'comment_form': comment_form
+    })
+    
+    
+    
 def post_update(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == 'POST':
@@ -22,9 +41,12 @@ def post_update(request, pk):
     return render(request, 'blog/post_form.html', {'form': form})
 
 
+
 def post_delete(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == 'POST':
         post.delete()
         return redirect('post_list')
     return render(request, 'blog/post_delete.html', {'post': post})
+
+
