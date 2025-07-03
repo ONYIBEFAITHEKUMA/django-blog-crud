c3393425c8b2ca27a07b62299b4f66634900eb62
ONYIBEFAITHEKUMA <ONYIBEFAITHEKUMA@gmail.com>
2025-07-03 16:54:32 +0100

    Add post update view, form and post form, to enable user edit a psot

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path("<int:pk>", views.post_detail, name="post_details")
+    path('<int:pk>/', views.post_detail, name='post_detail'),
+    path('<int:pk>/', views.post_detail, name='post_detail'),
+    path('<int:pk>/edit/', views.post_update, name='post_update'),
+
+
 ]
