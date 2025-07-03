d8970643917d8ce90983381fb177753e303e101a
ONYIBEFAITHEKUMA <ONYIBEFAITHEKUMA@gmail.com>
2025-07-03 16:00:03 +0100

    Add Add Post list view and URL routing for post list view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post
+
+def post_list(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'blog/post_list.html', {'posts': posts})
